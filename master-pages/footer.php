d76c48263d74630647dc7afdd5be91bbf562746a
Ciprian <ciprian_condurachi@yahoo.com>
2024-06-01 16:46:15 +0200

    responsive footer

--- master-pages/footer.php
+++ master-pages/footer.php
@@ -2,49 +2,48 @@
     <footer>
         <section class="px-lg-0 px-4 py-lg-5 py-4 bg-accent-color-1">
             <div class="r-container text-white">
-                <div class="row row-cols-1 row-cols-lg-4">
-                    <div class="col col-sm-6 col-lg-3 logo">
-                        <div class="d-flex flex-column h-100 justify-content-center">
-                            <div class="logo-container">
-                                <img src="<?=$us_url_root?>assets/images/logo-blue-background.svg" alt="Logo with blue background" class="img-fluid">
-                            </div>
-                        </div>
+                <div class="row row-cols-1">
+                    <div class="col col-sm-12 col-md-4 logo">
+                        <img src="<?=$us_url_root?>assets/images/logo-blue-background.svg" alt="Logo with blue background" class="img-fluid">
                     </div>
-                    <div class="col col-sm-6 col-lg-3 links">
-                        <div class="d-flex flex-column">
-                            <h5 class="font-1 fw-bold mb-3">Links</h5>
-                            <div class="d-flex flex-column gap-2">
-                                <a href="<?=$home_page_url?>" class="d-flex flex-row gap-2 align-items-center link-light">Home</a>
-                                <a href="#services-section" class="d-flex flex-row gap-2 align-items-center link-light">Ydelser</a>
-                                <a href="#courses-section" class="d-flex flex-row gap-2 align-items-center link-light">Kurser</a>
-                                <a href="#about-us-section" class="d-flex flex-row gap-2 align-items-center link-light">Om Os</a>
-                                <a href="#contact-section" class="d-flex flex-row gap-2 align-items-center link-light">Kontakt</a>
+                    <div class="col col-sm-6 col-md-4 links">
+                        <h5 class="font-1 fw-bold mb-3">Links</h5>
+                        <div class="d-flex">
+                            <div class="links-group">
+                                <a href="<?=$home_page_url?>" class="align-items-center">Home</a>
+                                <a href="#services-section" class="align-items-center">Ydelser</a>
+                                <a href="#courses-section" class="align-items-center">Kurser</a>
+                                <a href="#about-us-section" class="align-items-center">Om Os</a>
+                            </div>
+                            <div class="links-group">
+                                <a href="#contact-section" class="align-items-center">Kontakt</a>
+                                <a href="#" class="align-items-center">Cookies politik</a>
+                                <a href="#" class="align-items-center">Persondata politik</a>                            
                             </div>
                         </div>
                     </div>               
-                    <div class="col col-sm-12 col-lg-6 contact">
+                    <div class="col col-sm-6 col-md-4 contact">
                         <div class="d-flex flex-column mb-3">
                             <h5 class="font-1 fw-bold mb-1">Kontakt os</h5>
-                            <div class="d-flex flex-wrap">
-                                <div class="d-flex flex-row gap-2 align-items-center contact-detail">
-                                    <a class="location" target=_blank href="<?=$company_address_google_url?>">
-                                        <i class="fa-solid fa-location-dot"></i>
-                                        <?=$company_address?>
-                                    </a>
-                                </div>
-                                <div class="d-flex flex-row gap-2 align-items-center contact-detail">
-                                    <a class="phone" href="tel:<?=$company_phone?>">
-                                        <i class="fa-solid fa-phone"></i>
-                                        <?=$company_phone_display?>
-                                    </a>
-                                </div>
-                                <div class="d-flex flex-row gap-2 align-items-center contact-detail">
-                                    <a class="email" href="mailto:<?=$company_contact_email?>">
-                                        <i class="fa-solid fa-envelope"></i>
-                                        <?=$company_contact_email?>
-                                    </a>
-                                </div>
+                            <div class="align-items-center contact-detail">
+                                <a class="location" target=_blank href="<?=$company_address_google_url?>">
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    <?=$company_address?>
+                                </a>
                             </div>
+                            <div class="align-items-center contact-detail">
+                                <a class="phone" href="tel:<?=$company_phone?>">
+                                    <i class="fa-solid fa-phone"></i>
+                                    <?=$company_phone_display?>
+                                </a>
+                            </div>
+                            <div class="align-items-center contact-detail">
+                                <a class="email" href="mailto:<?=$company_contact_email?>">
+                                    <i class="fa-solid fa-envelope"></i>
+                                    <?=$company_contact_email?>
+                                </a>
+                            </div>
+                            
                         </div>
                         <div class="d-flex flex-column mb-3">
                             <div class="social-container">
